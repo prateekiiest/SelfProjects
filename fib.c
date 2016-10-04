c57d4ce57aa61f7d79858531f9ede5d37e29e571
Prateek Chanda <prateekkol21@gmail.com>
2016-10-04 06:41:42 -0400

    Add files via upload
    Adding the fibonacci sequence code.

---
+++ fib.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int n,i;
+printf("Enter the number for fibonacci sequence: ");
+scanf("%d",&n);  // Given Number
+
+int a[n];
+
+a[0] = 1;
+a[1] = 1;
+
+for(i=2;i<n;i++)
+{
+a[i] = (a[i-1] + a[i-2]);
+}
+
+
+for(i=0;i<n;i++){
+printf("%d ",a[i]);
+
+}
+
+
+
+return 0;
+
+
+
+
+
+
+}
+
+
+
